94c39ca78cb7fc5fc085ead6e325a931d0418b05
Yarli <y.matheus.borges@hotmail.com>
2025-11-19 22:45:50 +0000

    Completing refactoring, cleaning code with OOP

--- Ad_Web_Dev/Topic_1_Mine/topic3/bioweb/genedata/urls.py
+++ Ad_Web_Dev/Topic_1_Mine/topic3/bioweb/genedata/urls.py
@@ -2,15 +2,13 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('gene/<int:pk>', views.gene, name='gene'), # connect gene to tamplate
-    path('list/<str:type>', views.list, name='list'), # new path for list view
-    path('poslist/', views.poslist, name='poslist'),
-    path('delete/<int:pk>', views.delete, name='delete'), # new path for delete view
-    path('create_ec/', views.create_ec, name='create_ec'),
-    path('create_gene/', views.create_gene, name='create_gene'),
     path('', views.GeneList.as_view(), name='index'),
-    path('gene/<int:pk>', views.GeneDetail.as_view(), name='gene'),
+    path('gene/<int:pk>', views.GeneDetail.as_view(), name='gene'), # connect gene to tamplate
+    path('list/<str:type>', views.GeneList.as_view(), name='list'), # new path for list view
+    path('poslist/', views.GeneList.as_view(), name='poslist'),
+    path('delete/<int:pk>', views.GeneDelete.as_view(), name='delete'), # new path for delete view
+    path('create_ec/', views.create_ec, name='create_ec'),
     path('create_gene/', views.GeneCreate.as_view(), name='create_gene'),
-    path('delete/<int:pk>', views.GeneDelete.as_views(), name='delete'),
+    path('update/<int:pk>', views.GeneUpdate.as_view(), name='update'),
+    # path('list/<str:type>', views.GeneList.as_view(), name='list'),
 ]
--- Ad_Web_Dev/Topic_1_Mine/topic3/bioweb/genedata/views.py
+++ Ad_Web_Dev/Topic_1_Mine/topic3/bioweb/genedata/views.py
@@ -1,37 +1,122 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from django.views.generic import CreateView
+from django.views.generic import DeleteView
+from django.views.generic import UpdateView
+
 from .models import *
 from .forms import *
-from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, DeleteView
 
-def index(request):
-    master_genes = Gene.objects.all() # query Gene table
-    return render(request, 'genedata/index.html', {'master_genes': master_genes})
+class GeneList(ListView):
+    model = Gene
+    context_object_name = 'master_genes'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['master_genes'] = Gene.objects.all()
+        if "poslist" in self.request.get_full_path():
+            context['genes'] = Gene.objects.filter(entity__exact='Chromosome').filter(sense__startswith='+')
+        if 'type' in self.kwargs:
+            if "Chromosome" in self.kwargs['type'] or "Plasmid" in self.kwargs['type']:
+                context['genes'] = Gene.objects.filter(entity__exact=self.kwargs['type'])
+        return context
+            
+    def get_template_names(self):
+        if "poslist" in self.request.get_full_path():
+            return 'genedata/list.html'
+        if 'type' in self.kwargs:
+            if "Chromosome" in self.kwargs['type'] or "Plasmid" in self.kwargs['type']:
+                return 'genedata/list.html'
+        return 'genedata/index.html'
 
-def gene(request, pk):
-    gene = Gene.objects.get(pk=pk)
-    gene.access += 1
-    print("Gene record: ", pk, "access count:", str(gene.access))
-    gene.save()
-    master_genes = Gene.objects.all()
-    return render(request, 'genedata/gene.html', {'gene': gene, 'master_genes': master_genes})
+""" Replaced with the above class"""
+# def index(request):
+#     master_genes = Gene.objects.all() # query Gene table
+#     return render(request, 'genedata/index.html', {'master_genes': master_genes})
 
-def list(request, type):
-    genes = Gene.objects.filter(entity__exact=type)
-    master_genes = Gene.objects.all()
-    return render(request, 'genedata/list.html', {'genes': genes, 'type': type, 'master_genes': master_genes})
+# def list(request, type):
+#     genes = Gene.objects.filter(entity__exact=type)
+#     master_genes = Gene.objects.all()
+#     return render(request, 'genedata/list.html', {'genes': genes, 'type': type, 'master_genes': master_genes})
 
-def poslist(request):
-    genes = Gene.objects.filter(entity__exact='Chromosome').filter(sense__startswith='+')
-    master_genes = Gene.objects.all()
-    return render(request, 'genedata/list.html', {'genes': genes, 'type': 'PosList', 'master_genes': master_genes})
+# def poslist(request):
+#     genes = Gene.objects.filter(entity__exact='Chromosome').filter(sense__startswith='+')
+#     master_genes = Gene.objects.all()
+#     return render(request, 'genedata/list.html', {'genes': genes, 'type': 'PosList', 'master_genes': master_genes})
 
-def delete(request, pk):
-    GeneAttributeLink.objects.filter(gene_id=pk).delete()
-    Gene.objects.filter(pk=pk).delete()
-    return HttpResponseRedirect('/')
 
+class GeneDetail(DetailView):
+    model = Gene
+    context_object_name = 'gene'
+    template_name = 'genedata/gene.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['master_genes'] = Gene.objects.all()
+        return context
+  
+""" Replaced with the above class"""  
+# def gene(request, pk):
+#     gene = Gene.objects.get(pk=pk)
+#     gene.access += 1
+#     print("Gene record: ", pk, "access count:", str(gene.access))
+#     gene.save()
+#     master_genes = Gene.objects.all()
+#     return render(request, 'genedata/gene.html', {'gene': gene, 'master_genes': master_genes})
+
+
+class GeneDelete(DeleteView):
+    model = Gene
+    success_url = '/'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['master_genes'] = Gene.objects.all()
+        return context
+    
+"""Replaced with the above class"""
+# def delete(request, pk):
+#     GeneAttributeLink.objects.filter(gene_id=pk).delete()
+#     Gene.objects.filter(pk=pk).delete()
+#     return HttpResponseRedirect('/')
+
+class GeneCreate(CreateView):
+    model = Gene
+    template_name = 'genedata/create_gene.html'
+    form_class = GeneForm
+    success_url = '/create_gene/'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['master_genes'] = Gene.objects.all()
+        return context
+    
+"""Replaced with the above class"""
+# def create_gene(request):
+#     master_genes = Gene.objects.all()
+#     if request.method == 'POST':
+#         form = GeneForm(request.POST)
+#         if form.is_valid():
+#             gene = form.save()
+#             return HttpResponseRedirect('/create_gene/')
+#         else:
+#             return render(request, 'genedata/create_gene.html', {'error': 'failed', 'master_genes': master_genes, 'form': form})
+#     else: 
+#         form = GeneForm()
+#     return render(request, 'genedata/create_gene.html', {'form': form, 'master_genes': master_genes})
+
+class GeneUpdate(UpdateView):
+    model = Gene
+    fields = fields = ['gene_id', 'entity', 'start', 'stop', 'sense', 'start_codon', 'sequencing', 'ec']
+    template_name_suffix = '_update_form'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['master_genes'] = Gene.objects.all()
+        return context
+    
 def create_ec(request):
     master_genes = Gene.objects.all()
     ecs = EC.objects.all()
@@ -47,53 +132,3 @@ def create_ec(request):
     else:
         form = ECForm()
     return render(request, 'genedata/ec.html', {'form': form,'ecs': ecs, 'master_genes': master_genes})
-
-def create_gene(request):
-    master_genes = Gene.objects.all()
-    if request.method == 'POST':
-        form = GeneForm(request.POST)
-        if form.is_valid():
-            gene = form.save()
-            return HttpResponseRedirect('/create_gene/')
-        else:
-            return render(request, 'genedata/create_gene.html', {'error': 'failed', 'master_genes': master_genes, 'form': form})
-    else: 
-        form = GeneForm()
-    return render(request, 'genedata/create_gene.html', {'form': form, 'master_genes': master_genes})
-
-
-class GeneList(ListView):
-    model = Gene
-    context_obejct_name = 'master_genes'
-    template_name = 'genedata/index.html'
-    
-class GeneDetail(DetailView):
-    model = Gene
-    context_object_name = 'gene'
-    template_name = 'genedata/gene.html'
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['master_genes'] = Gene.objects.all()
-        return context
-    
-class GeneCreate(CreateView):
-    model = Gene
-    template_name = 'genedata/create_gene.html'
-    form_class = GeneForm
-    success_url = '/create_gene/'
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['master_genes'] = Gene.objects.all()
-        return context
-    
-class GeneDelete(DeleteView):
-    model = Gene
-    success_url = '/'
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['master_genes'] = Gene.objects.all()
-        return context
-    
